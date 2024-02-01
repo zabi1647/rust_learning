f2890b6d3baff7bc06a2eac24dd4b3b32fc631e9
Zohaib <101661680+zabi1647@users.noreply.github.com>
2024-02-01 19:40:37 +0500

    data types

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, zohaib");
+    // let x:i8 = 12;
+    // let y = 02_55;
+    let z:[i32;3] = [1,2,3];
+    
+    // println!("the value of x is {}",x);
+    // println!("the value of y is {}",y);
+    println!("the value of z is {}",z[2]);
 }
